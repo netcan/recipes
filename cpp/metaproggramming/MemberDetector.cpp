6f711f9a9759c451b1ae29d5bbee2e1797328b3b
netcan <1469709759@qq.com>
2021-03-17 20:00:06 +0800

    revert member if

--- cpp/metaproggramming/MemberDetector.cpp
+++ cpp/metaproggramming/MemberDetector.cpp
@@ -37,19 +37,62 @@ constexpr bool IsDeref<std::unique_ptr<Ts...>> = false;
 template<typename T, typename Detector,
     typename Ret = std::decay_t<decltype(std::declval<Detector>()(std::declval<T>()))>,
     typename D = Ret>
-constexpr decltype(auto) MemberWrapper(T& v, Detector detector, ExistField, D&& = {}) { 
+constexpr decltype(auto) MemberWrapper(T& v, Detector detector, ExistField, D&& = {}) {
     static_assert(IsDeref<T>, "value must be deref!");
-    return detector(v); 
+    return detector(v);
 }
 
 // not detect field, noop
 
 template<typename T, typename Detector, typename D = DummyV>
-constexpr decltype(auto) MemberWrapper(T&, Detector, NoField, D&& defaut = {}) { 
+constexpr decltype(auto) MemberWrapper(T&, Detector, NoField, D&& defaut = {}) {
     static_assert(IsDeref<T>, "value must be deref!");
-    return std::forward<D>(defaut); 
+    return std::forward<D>(defaut);
 }
 
+
+///////////////////////////////////////////////////////////////////////////////
+// If a member isn't exist, invoke `then'; exist invoke `alternate'
+template<typename T, typename Then, typename Alternate>
+struct MemberExistIfBase {
+    constexpr MemberExistIfBase(
+            T& value,
+            Then then,
+            Alternate alternate):
+        value(value),
+        then(std::move(then)),
+        alternate(std::move(alternate)) {};
+
+    T& value;
+    Then then;
+    Alternate alternate;
+};
+
+template<typename T,
+    typename Then, typename Alternate,
+    typename = void>
+struct MemberExistIf
+    : private MemberExistIfBase<T, Then, Alternate> {
+    using Base = MemberExistIfBase<T, Then, Alternate>;
+    using Base::Base;
+    constexpr void operator()() { Base::alternate(Base::value); }
+};
+
+template<typename T,
+    typename Then, typename Alternate>
+struct MemberExistIf<T, Then, Alternate,
+    std::void_t<decltype(std::declval<Then>()(std::declval<T>()))>>
+    : private MemberExistIfBase<T, Then, Alternate> {
+    using Base = MemberExistIfBase<T, Then, Alternate>;
+    using Base::Base;
+    constexpr void operator()() { Base::then(Base::value); }
+};
+
+// C++17 deduction guide
+template<typename T,
+    typename Then, typename Alternate>
+MemberExistIf(T, Then, Alternate)
+    -> MemberExistIf<T, Then, Alternate>;
 }
 
 // MemWrap(_value, _field, [default value])
@@ -61,6 +104,19 @@ constexpr decltype(auto) MemberWrapper(T&, Detector, NoField, D&& defaut = {}) {
             details::ExistField{},    \
             ##__VA_ARGS__)
 
+
+#define MemIf(_value, _field, _then, _alternate)       \
+    details::MemberExistIf(_value,                     \
+        [](auto&& _value) -> decltype(_value._field) { \
+            _then                                      \
+            return _value._field;                      \
+        },                                             \
+        [](auto&& _value){                             \
+            _alternate                                 \
+        })()
+
+#define MemThen(...) __VA_ARGS__
+#define MemElse(...) __VA_ARGS__
 
 int main(int argc, char** argv) {
     struct Msg {
@@ -79,12 +135,12 @@ int main(int argc, char** argv) {
         MemWrap(msg, a) = 1;
         MemWrap(msg, b) = 2;
         MemWrap(msg, c) = 3; // noop
-    
+
                 msg .nest.d[0].c[0]  = 4;
         MemWrap(msg, nest.d[0].c[0]) = 4;
         MemWrap(msg, nest.d[0].e) = 4; // noop
-    
-        printf("msg.a = %d msg.b = %d msg.nest = %d\n", 
+
+        printf("msg.a = %d msg.b = %d msg.nest = %d\n",
                 msg.a, msg.b, msg.nest.d[0].c[0]);
     }
 
@@ -94,14 +150,14 @@ int main(int argc, char** argv) {
         MemWrap(msg, a) = 1;
         MemWrap(msg, b) = 2;
         MemWrap(msg, nest.d[0].c[0]) = 4;
-    
+
         printf("msg.a = %d msg.b = %d "
-               "msg.nest = %d msg.noexist = %d\n", 
-                MemWrap(msg, a, -1), 
-                MemWrap(msg, b, -1), 
+               "msg.nest = %d msg.noexist = %d\n",
+                MemWrap(msg, a, -1),
+                MemWrap(msg, b, -1),
                 MemWrap(msg, nest.d[0].c[0], -1),
                 MemWrap(msg, noexist, -1));
-    
+
     }
 
     // static check
@@ -110,6 +166,48 @@ int main(int argc, char** argv) {
         // error: static assertion failed: value must be deref!
         // MemWrap(msg, a) = 1;
         MemWrap(*msg, a) = 1;
+    }
+
+    {
+        Msg msg{0, 0};
+
+        MemWrap(msg, a) = 1;
+        MemWrap(msg, b) = 2;
+        MemWrap(msg, c) = 3; // noop
+
+                msg .nest.d[0].c[0]  = 4;
+        MemWrap(msg, nest.d[0].c[0]) = 4;
+        MemWrap(msg, nest.d[0].e) = 4; // noop
+
+        std::cout << "msg.a = " << msg.a
+            << "; msg.b = " << msg.b
+            << "; msg.nest = " << msg.nest.d[0].c[0]
+            << std::endl;
+
+        MemIf(msg, c,
+            MemThen(
+                msg.c += 10;
+                puts("msg.c in msg!");
+            ),
+            MemElse(
+                puts("msg.c not in msg!");
+            )
+        );
+
+        MemIf(msg, a,
+            MemThen(
+                msg.a += 10;
+                puts("msg.a in msg!");
+            ),
+            MemElse(
+                puts("msg.a not in msg!");
+            )
+        );
+
+        std::cout << "msg.a = " << msg.a
+            << "; msg.b = " << msg.b
+            << "; msg.nest = " << msg.nest.d[0].c[0]
+            << std::endl;
     }
 
     return 0;
