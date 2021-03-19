1b1b7a245b128c5781a3171442ff0ab4859b7ceb
netcan <1469709759@qq.com>
2021-03-19 22:27:24 +0800

    update reflection: add deserialize

--- cpp/metaproggramming/reflection/StaticRefl.cpp
+++ cpp/metaproggramming/reflection/StaticRefl.cpp
@@ -8,27 +8,52 @@
 ************************************************************************/
 #include <cstdio>
 #include <iostream>
+#include <sstream>
 #include "StaticRefl.hpp"
 
 template<typename T>
-void dumpObj(T&& obj, const char* fieldName = "", int depth = 0) {
-    auto indent = [depth] {
-        for (int i = 0; i < depth; ++i) {
-            std::cout << "    ";
-        }
+void serializeObj(std::ostream& out, T&& obj,
+        const char* fieldName = "", int depth = 0) {
+    auto indent = [&] {
+        for (int i = 0; i < depth; ++i)
+        { out << "    "; }
     };
 
     if constexpr(IsReflected_v<std::decay_t<T>>) {
         indent();
-        std::cout << fieldName << (*fieldName ? ": {" : "{") << std::endl;
-        forEach(obj, [depth](auto&& fieldName, auto&& value) {
-            dumpObj(value, fieldName, depth + 1);
-        });
+        out << fieldName << (*fieldName ? ": {" : "{") << std::endl;
+        forEach(std::forward<T>(obj),
+                [&](auto&& fieldName, auto&& value)
+                { serializeObj(out, value, fieldName, depth + 1); });
         indent();
-        std::cout << "}" << (depth == 0 ? "" : ",") << std::endl;
+        out << "}" << std::endl;
     } else {
         indent();
-        std::cout << fieldName << ": " << obj << "," << std::endl;
+        out << fieldName << ": " << obj << std::endl;
+    }
+}
+
+template<typename T>
+void deserializeObj(std::istream& in, T&& obj,
+        const char* fieldName = "") {
+    if constexpr(IsReflected_v<std::decay_t<T>>) {
+        std::string token;
+        in >> token; // eat '{'
+        if (*fieldName) {
+            in >> token; // WARNING: needs check fieldName valid
+        }
+
+        forEach(std::forward<T>(obj),
+                [&](auto&& fieldName, auto&& value)
+                { deserializeObj(in, value, fieldName); });
+
+        in >> token; // eat '}'
+    } else {
+        if (*fieldName) {
+            std::string token;
+            in >> token; // WARNING: needs check fieldName valid
+        }
+        in >> obj; // dump value
     }
 }
 
@@ -42,12 +67,22 @@ DEFINE_STRUCT(Rect,
         (uint32_t) color)
 
 int main(int argc, char** argv) {
-    Rect rect {
-        {1.2, 3.4},
-        {5.6, 7.8},
-        12345678,
-    };
-    dumpObj(rect);
+    std::stringstream result; // serialize result
+    {
+        Rect rect {
+            {1.2, 3.4},
+            {5.6, 7.8},
+            12345678,
+        };
+        serializeObj(result, rect);
+    }
+    std::cout << "serialize rect result:" << std::endl
+        << result.str() << std::endl;
+
+    Rect rect2;
+    deserializeObj(result, rect2);
+    std::cout << "deserialize rect result:" << std::endl;
+    serializeObj(std::cout, rect2);
 
     return 0;
 }
